12166b365daa61f4708a97e144dc4e5dfb028b49
Krista Dotzenrod <kdotzenrod@Mac.lan>
2025-10-25 20:11:31 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Register from "./components/Register";
+import Admin from "./components/Admin";
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div><h1>Wholly Roasters</h1></div>
+
+  <ul id="navbar">
+    <li className="navitem"><Link to="" className="active">Home</Link></li>
+    <li className="navitem"><Link to="/about">About</Link></li>
+    <li className="navitem"><Link to="/shop">Shop</Link></li>
+    <li className="navitem"><Link to="/register">Register</Link></li>
+    <li className="navitem"><Link to="/admin">Admin</Link></li>
+  </ul>
+
+   <div className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+
+   </div>    
+
+
+
     </div>
   );
 }
